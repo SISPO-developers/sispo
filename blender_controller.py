2232df555f47000c70d978aaee8ddb61c7396a2d
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2019-04-24 12:24:21 +0300

    Rename object to obj, as object is "python" name

--- blender_controller.py
+++ blender_controller.py
@@ -147,14 +147,14 @@ class BlenderController:
             data_to.objects = [
                 name for name in data_from.objects if name == object_name]
         if len(data_to.objects) > 0:
-            object = data_to.objects[0]
-            object.animation_data_clear()
+            obj = data_to.objects[0]
+            obj.animation_data_clear()
             if len(scene_names) == 0:
                 scene_names = self.scene_names
             for scene_name in scene_names:
                 scene = bpy.data.scenes[scene_name]
-                scene.collection.objects.link(object)
-            return object
+                scene.collection.objects.link(obj)
+            return obj
         return None
 
     def set_camera(self, lens=35, sensor=32, clip_start=1E-5, clip_end=1E32, mode="PERSP",
