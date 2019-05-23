90f41bb1cc5198eb056eceb17ab5199563364ebe
YgabrielsY <gabriel.schwarzkopf@yahoo.de>
2019-05-23 08:30:03 +0300

    Remove orekit data from root directory

---
+++ build/data/download_orekit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Downloading orekit-data.zip start"
+
+# Create folder
+cd ../..
+[[ -d data ]] || mkdir data
+cd data
+
+[[ -d orekit ]] || mkdir orekit
+cd orekit
+
+# Download data
+wget https://gitlab.orekit.org/orekit/orekit-data/-/archive/master/orekit-data-master.zip
+mv orekit-data-master.zip orekit-data.zip
+
+echo "Downloading orekit-data.zip done"
