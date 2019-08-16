f55620e6c6473cb6ab28650757c06ecc8c939060
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2019-08-16 22:07:18 +0300

    Add main module file with docstring

---
+++ vina/vina.py
@@ -0,0 +1,9 @@
+"""
+The vina package provides functions to do VIsual NAvigation studies
+
+The package creates images of a 3D object using blender. The images are render
+in a flyby scenario. UCAC4 star catalogue to create the background. Afterwards
+hese images are used with openMVG and openMVS to reconstruct the 3D model and
+reconstruct the trajectory.
+"""
+
