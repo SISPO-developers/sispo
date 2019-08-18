5eedec2ba29e8396aff59a6ce34cb1e53424c8dc
Schwarzkopf Gabriel <schwarg1@login2.triton.aalto.fi>
2019-08-18 20:57:06 +0300

    update setup_logged.sh

--- build/setup_logged.sh
+++ build/setup_logged.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -euo pipefail
+exec 2>&1
 
 echo "Setup with logging what's shappening start"
 
-./setup.sh > log.txt 2> error_log.txt
+./setup.sh | tee setup.log
 
 echo "Setup with logging what's happening done"
