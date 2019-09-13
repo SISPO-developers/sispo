03494ba29346a90d1849257c05d91cd637938691
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2019-09-13 15:06:14 +0300

    Add resolve and create directory function to utils

--- sispo/utils.py
+++ sispo/utils.py
@@ -1,1 +1,12 @@
+"""Utils module contains functions possibly used by all modules."""
 
+import pathlib
+
+def resolve_create_dir(directory):
+    """Resolves directory and creates it, if it doesn't existing."""
+    dir_resolved = directory.resolve()
+
+    if not dir_resolved.exists():
+        Path.mkdir(dir_resolved)
+
+    return dir_resolved
