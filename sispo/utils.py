8f2c0eb33a2c40aef20d4c8daa08bed922059c15
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2019-09-13 15:09:43 +0300

    Add write vec and mat into string functions to utils

--- sispo/utils.py
+++ sispo/utils.py
@@ -2,6 +2,7 @@
 
 import pathlib
 
+
 def resolve_create_dir(directory):
     """Resolves directory and creates it, if it doesn't existing."""
     dir_resolved = directory.resolve()
@@ -10,3 +11,27 @@ def resolve_create_dir(directory):
         Path.mkdir(dir_resolved)
 
     return dir_resolved
+
+
+def write_vec_string(vec, prec):
+    """Write data vector into string."""
+    o = "["
+    fs = "%%.%de" % (prec)
+    #i = 0
+    for (n, v) in enumerate(vec):
+
+        o += fs % (v)
+        if n < len(vec) - 1:
+            o += ","
+    return o + "]"
+
+
+def write_mat_string(vec, prec):
+    """Write data matrix into string."""
+    o = "["
+    for (n, v) in enumerate(vec):
+
+        o += (write_vec_string(v, prec))
+        if n < len(vec) - 1:
+            o += ","
+    return o + "]"
