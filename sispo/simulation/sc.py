9c2845d77ba9d367c1dabc79452bd8a605e65f8a
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2019-09-23 11:43:00 +0300

    Add CB superclass

--- sispo/simulation/sc.py
+++ sispo/simulation/sc.py
@@ -15,20 +15,22 @@ from org.orekit.frames import FramesFactory # pylint: disable=import-error
 from org.orekit.propagation.analytical import KeplerianPropagator # pylint: disable=import-error
 from org.orekit.time import AbsoluteDate, TimeScalesFactory # pylint: disable=import-error
 
-class Spacecraft():
+from sispo.simulation.cb import CelestialBody
+
+
+class Spacecraft(CelestialBody):
     """Handling properties and behaviour of the spacecraft."""
 
-    def __init__(self, ROOT_DIR_PATH):
+    def __init__(self, name, ref_frame):
         """Currently hard implemented for SC."""
+
+        super().__init__(name, ref_frame)
 
         self.frame = FramesFactory.getICRF()
         mu_sun = utils.Constants.IAU_2015_NOMINAL_SUN_GM
 
-        self.orbit = orbits.KeplerianOrbit(self.a, self.e, self.i, self.omega, self.Omega, self.M,
+        self.trajectory = orbits.KeplerianOrbit(self.a, self.e, self.i, self.omega, self.Omega, self.M,
                                   orbits.PositionAngle.MEAN, self.frame, date_initial, mu_sun)
-        self.propagator = KeplerianPropagator(self.orbit)
+        self.propagator = KeplerianPropagator(self.trajectory)
 
         self.pos_history = []
-
-        self.pos = None
-        self.vel = None
