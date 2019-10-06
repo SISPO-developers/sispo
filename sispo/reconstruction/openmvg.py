1f3e8a06dd9b6d405171bdbf0fbf0ef4412cc049
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2019-10-06 12:08:54 +0300

    Initial commit for openmvg controller #35

--- sispo/reconstruction/openmvg.py
+++ sispo/reconstruction/openmvg.py
@@ -1,1 +1,20 @@
+"""Class to control openMVG behaviour."""
 
+from pathlib import Path
+
+
+class OpenMVGControllerError(RuntimeError):
+    """Generic openMVG error."""
+    pass
+
+
+class OpenMVGController():
+    """Controls behaviour of openMVG data processing."""
+
+    def __init__(self, res_dir):
+        
+        self.root_dir = Path(__file__).parent.parent.parent
+        self.openMVG_dir = self.root_dir / "software" / "openMVG" / "build_openMVG"
+        self.openMVG_dir = self.openMVG_dir / "Windows-AMD64-Release" / "Release"
+
+        self.res_dir = res_dir
