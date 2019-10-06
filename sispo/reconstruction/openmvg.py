b853cbe1d26bfb1014831b8e78d7923ab7ceb705
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2019-10-06 12:11:06 +0300

    Add logger #35

--- sispo/reconstruction/openmvg.py
+++ sispo/reconstruction/openmvg.py
@@ -2,6 +2,9 @@
 
 from pathlib import Path
 
+import utils
+
+logger = utils.logger("openmvg")
 
 class OpenMVGControllerError(RuntimeError):
     """Generic openMVG error."""
@@ -16,5 +19,7 @@ class OpenMVGController():
         self.root_dir = Path(__file__).parent.parent.parent
         self.openMVG_dir = self.root_dir / "software" / "openMVG" / "build_openMVG"
         self.openMVG_dir = self.openMVG_dir / "Windows-AMD64-Release" / "Release"
+
+        logger.info("openMVG executables dir %s", str(self.openMVG_dir))
 
         self.res_dir = res_dir
