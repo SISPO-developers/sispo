28d283b0a942e8b6341dae8b1e67c8834297bdbe
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2019-10-07 11:35:09 +0300

    Add point cloud densification #37

--- sispo/reconstruction/openmvs.py
+++ sispo/reconstruction/openmvs.py
@@ -24,3 +24,19 @@ class OpenMVSController():
 
         self.res_dir = res_dir
 
+    def densify_pointcloud(self):
+        """Increases number of points to make 3D model smoother."""
+        logger.info("Densify point cloud to make model smoother")
+
+        self.export_dir = utils.check_dir(self.res_dir / "export")
+        self.export_scene = self.export_dir / "scene.mvs"
+
+        exe = str(self.openMVS_dir / "DensifyPointCloud")
+
+        ret = subprocess.run([exe,
+                              "-i", str(self.export_scene),
+                              "--max-threads", "0",
+                              "--estimate-normals", "1",
+                              "--number-views", "0",
+                              "-v", "3"])#,"--number-views-fuse","5"] )
+        logger.info("Point cloud densification returned: %s", str(ret))
