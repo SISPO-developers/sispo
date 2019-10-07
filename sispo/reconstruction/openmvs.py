9b7aa259ca04a4d297d4ad821f35150d690c417a
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2019-10-07 11:27:44 +0300

    Add openMVS controller and generic error #37

--- sispo/reconstruction/openmvs.py
+++ sispo/reconstruction/openmvs.py
@@ -1,1 +1,26 @@
+"""Class to control openMVS behaviour."""
+
+from pathlib import Path
+import subprocess
+
+import utils
+
+logger = utils.create_logger("openmvs")
+
+
+class OpenMVSControllerError(RuntimeError):
+    """Generic openMVS error."""
+    pass
+
+
+class OpenMVSController():
+    """Controls behaviour of openMVS data processing."""
+
+    def __init__(self, res_dir):
+        """."""
+        self.root_dir = Path(__file__).parent.parent.parent
+        self.openMVS_dir = self.root_dir / "software" / "openMVS" / "build"
+        self.openMVS_dir = self.openMVS_dir / "bin" / "x64" / "Debug"
+
+        self.res_dir = res_dir
 
