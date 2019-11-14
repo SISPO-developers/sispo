6b392000ae9f4669a4134d471b82fb9b99451fea
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2019-11-14 15:37:45 +0200

    Add compression test code

--- sispo/sispo.py
+++ sispo/sispo.py
@@ -7,8 +7,28 @@ hese images are used with openMVG and openMVS to reconstruct the 3D model and
 reconstruct the trajectory.
 """
 
+from pathlib import Path
+
 import simulation.simulation as sim
 import reconstruction.reconstruction as rc
+import compression.compression as comp
+import utils
+
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    rec = rc.Reconstructor()
+    root_dir = Path(__file__).parent.parent
+    name = "Didymos"
+    res_dir = utils.check_dir(root_dir / "data" / "results" / name)
+    compr = comp.Compressor(res_dir)
+    compr.get_frame_ids()
+    compr.load_images()
+    compr.compress()
+    decomp = compr.decompress()
+
+    plt.imshow(compr.imgs[0])
+    plt.show()
+    plt.imshow(decomp[0])
+    plt.show()
+
+    print((compr.imgs[0] == decomp[0]).all())
