9fa55f6aaa37c2849ae9d90099250a0df0f9c243
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2020-01-08 13:13:25 +0200

    Working benchmark, using min Save diff image Output statistics

--- benchmarks/img_proc.py
+++ benchmarks/img_proc.py
@@ -5,6 +5,7 @@ Compares image resizing and gaussian filtering.
 
 from datetime import datetime
 from pathlib import Path
+import time
 import sys
 
 import cv2
@@ -20,9 +21,11 @@ logger_formatter = logging.Formatter(
     "%(asctime)s - %(name)s - %(funcName)s - %(message)s")
 
 now = datetime.now().strftime("%Y-%m-%dT%H%M%S%z")
-filename = (now + "_cv_skimage.log")
-log_dir = Path(".").resolve()
-log_file = log_dir / filename
+filename = "cv_skimage.log"
+res_dir = Path(".").resolve()
+res_dir = res_dir / now
+Path.mkdir(res_dir)
+log_file = res_dir / filename
 file_handler = logging.FileHandler(str(log_file))
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(logger_formatter)
@@ -32,26 +35,19 @@ stream_handler.setLevel(logging.DEBUG)
 stream_handler.setFormatter(logger_formatter)
 logger.addHandler(stream_handler)
 
-def benchmark_cv(image, sigma, kernel):
+def run_cv(image, sigma, kernel):
     """Run benchmark of OpenCV."""
-    result = np.zeros(image.shape, dtype=np.float32)
-    result = cv2.GaussianBlur(image, (kernel, kernel), sigma, sigma)
+    return cv2.GaussianBlur(image, (kernel, kernel), sigma, sigma)
 
-    return result
-
-def benchmark_skimage(image, sigma, trunc):
+def run_skimage(image, sigma, trunc):
     """Run benchmark of scikit-image."""
-    result = np.zeros(image.shape, dtype=np.float32)
-    result = skimage.filters.gaussian(image, sigma, truncate=trunc, multichannel=True)
+    return skimage.filters.gaussian(image, sigma, truncate=trunc, multichannel=True)
 
-    return result
-
-def run(filepath):
+def benchmark(filepath, iterations=10000):
     """Executes benchmark."""
     sigma = 5
     kernel = 5
     trunc = (kernel - 1) / 2 / sigma
-    iterations = 10
 
     logger.debug("Starting opencv vs skimage benchmarking")
     logger.debug(f"Using image {filepath}")
@@ -59,38 +55,43 @@ def run(filepath):
     logger.debug(f"Iterations: #{iterations}")
 
     raw_img = read_openexr_image(filepath)
-    
-    start = datetime.now()
+
+    times_sk = []
     for _ in range(iterations):
-        benchmark_skimage(raw_img, sigma, trunc)
-    end = datetime.now()
+        start = time.time()
+        run_skimage(raw_img, sigma, trunc)
+        end = time.time()
+        times_sk.append(end - start)
 
-    time_skimage = end - start
-
-    start = datetime.now()
+    times_cv = []
     for _ in range(iterations):
-        benchmark_cv(raw_img, sigma, kernel)
-    end = datetime.now()
+        start = time.time()
+        run_cv(raw_img, sigma, kernel)
+        end = time.time()
+        times_cv.append(end - start)
 
-    time_cv = end - start
+    time_skimage = min(times_sk)
+    time_cv = min(times_cv)
 
-    
-    logger.debug(f"skimage timing: {time_skimage / iterations} s")
-    logger.debug(f"OpenCV timing: {time_cv / iterations} s")
-    logger.debug(f"Ratio skimage/OpenCV: {time_skimage / time_cv}")
+    logger.debug(f"skimage timing: {time_skimage} s")
+    logger.debug(f"OpenCV timing: {time_cv} s")
+    logger.debug(f"Timing ratio skimage/OpenCV: {time_skimage / time_cv}")
 
-    cmd_skimage += "\n" + "print('SK type: ', sk_image.dtype)"
-    cmd_cv2 += "\n" + "print('CV2 type: ', cv_image.dtype)"
+    img_skimage = run_skimage(raw_img, sigma, trunc)
+    img_cv = run_cv(raw_img, sigma, kernel)
 
-    exec(setup_skimage + "\n" + cmd_skimage + "\n" + "utils.write_openexr_image(str(file) + '_sk', sk_image)")
-    exec(setup_cv2 + "\n" + cmd_cv2 + "\n" + "utils.write_openexr_image(str(file) + '_cv', cv_image)")
+    write_openexr_image(res_dir / "skimage.exr", img_skimage)
+    write_openexr_image(res_dir / "opencv.exr", img_cv)
 
+    diff = img_skimage - img_cv
+    logger.debug(f"Difference min: {np.min(diff)}; max: {np.max(diff)}")
+    write_openexr_image(res_dir / "diff.exr", diff)
 
-    statistics = setup_skimage + "\n" + setup_cv2 + "\n" + cmd_skimage + "\n" + cmd_cv2
-    statistics += "\n" + "diff = sk_image - cv_image"
-    statistics += "\n" + "print('Diff min, max: ', np.min(diff), np.max(diff))"
-    statistics += "\n" + "print('Alpha min sk, cv; max, sk, cv: ', np.min(sk_image[:,:,3]), np.min(cv_image[:,:,3]), np.max(sk_image[:,:,3]), np.max(cv_image[:,:,3]))"
-    exec(statistics)
+    equality = img_skimage[:,:,0:2] == img_cv[:,:,0:2]
+    logger.debug(f"Equality all: {equality.all()}; any: {equality.any()}")
+
+    logger.debug(f"Image skimage min: {np.min(img_skimage)}; max: {np.max(img_skimage)}")
+    logger.debug(f"Image OpenCV min: {np.min(img_cv)}; max: {np.max(img_cv)}")
 
 def read_openexr_image(filename):
     """Read image in OpenEXR file format into numpy array."""
@@ -130,10 +131,48 @@ def read_openexr_image(filename):
 
     return image_o
 
+
+def write_openexr_image(filename, image):
+    """Save image in OpenEXR file format from numpy array."""
+    height = len(image)
+    width = len(image[0])
+    channels = len(image[0][0])
+
+    # Default header only has RGB channels
+    hdr = OpenEXR.Header(width, height)
+
+    if channels == 4:
+        data_r = image[:, :, 0].tobytes()
+        data_g = image[:, :, 1].tobytes()
+        data_b = image[:, :, 2].tobytes()
+        data_a = image[:, :, 3].tobytes()
+        image_data = {"R": data_r, "G": data_g, "B": data_b, "A": data_a}
+        # Add alpha channel to header
+        alpha_channel = {"A": Imath.Channel(Imath.PixelType(OpenEXR.FLOAT))}
+        hdr["channels"].update(alpha_channel)
+
+    elif channels == 3:
+        data_r = image[:, :, 0].tobytes()
+        data_g = image[:, :, 1].tobytes()
+        data_b = image[:, :, 2].tobytes()
+        image_data = {"R": data_r, "G": data_g, "B": data_b}
+    else:
+        raise RuntimeError("Invalid number of channels of starmap image.")
+
+    file_handler = OpenEXR.OutputFile(str(filename), hdr)
+    file_handler.writePixels(image_data)
+    file_handler.close()
+
 if __name__ == "__main__":
+    args = {}
     try:
-        img_path = Path(sys.argv[1]).resolve()
+        args["filepath"] = Path(sys.argv[1]).resolve()
     except Exception as e:
         raise RuntimeError("Include filepath as argument")
 
-    run(img_path)
+    try:
+        args["iterations"] = int(sys.argv[2])
+    except Exception as e:
+        logger.debug("No number of iterations given")
+
+    benchmark(**args)
