ab674efeb0988dcfad930cd08198dfdcdd1e8215
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2020-03-01 18:46:26 +0200

    Updated setup to include doc build options #62

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+try:
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+except Exception:
+    long_description = "README file could not be opened"
 
 setuptools.setup(
     name="sispo",
-    version="0.1.0",
+    version="0.1.1",
     description="Space Imaging Simulator for Proximity Operations",
     long_description=long_description,
     platforms=["Windows"],
@@ -52,5 +55,12 @@ setuptools.setup(
         "Operating System :: Microsoft :: Windows :: Windows 10",
         "Operating System :: POSIX :: Linux",
         "Development Status :: 2 - Pre-Alpha"
-    ]
+    ],
+
+    command_options={
+        'build_sphinx': {
+            'source_dir': ('setup.py', 'doc/source'),
+            'build_dir': ('setup.py', 'doc/build')
+        }
+    }
 )
