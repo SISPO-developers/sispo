c82001b878744988ee6c8cce70dc4581095192e0
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2020-03-05 23:08:31 +0200

    Redo interface implementation initial commit #151

--- sispo/sispo.py
+++ sispo/sispo.py
@@ -8,8 +8,8 @@ reconstruct the trajectory.
 """
 
 import argparse
-from datetime import datetime
 import cProfile
+from datetime import datetime
 import io
 import json
 import logging
@@ -24,115 +24,84 @@ import pstats
 import sys
 import time
 
-from .sim import *
-from .sim import utils
-from .reconstruction import *
 from .compression import *
+from .reconstruction import *
+from .sim import utils
+from .sim import *
 
 logger = logging.getLogger("sispo")
 logger.setLevel(logging.DEBUG)
 logger_formatter = logging.Formatter(
     "%(asctime)s - %(name)s - %(funcName)s - %(message)s")
 
-parser = argparse.ArgumentParser(description=__file__.__doc__)
-parser.add_argument("-i",
-                    action="store",
-                    default=None,
-                    type=str, 
-                    help="Definition file")
-parser.add_argument("-v",
-                    action="store_false",
-                    help="Verbose output, displays log also on STDOUT")
-parser.add_argument("--cli",
-                    action="store_true",
-                    help="If set, starts interactive cli tool")
-parser.add_argument("--no-sim",
-                    action="store_const",
-                    const=False,
-                    default=True,
-                    dest="with_sim",
-                    help="If set, sispo will not simulate the scenario")
-parser.add_argument("--no-render",
-                    action="store_const",
-                    const=False,
-                    default=True,
-                    dest="with_render",
-                    help="If set, sispo will not render the scenario")
-parser.add_argument("--no-compression",
-                    action="store_const",
-                    const=False,
-                    default=True,
-                    dest="with_compression",
-                    help="If set, images will not be compressed after rendering")
-parser.add_argument("--no-reconstruction",
-                    action="store_const",
-                    const=False,
-                    default=True,
-                    dest="with_reconstruction",
-                    help="If set, no 3D model will be reconstructed")
-parser.add_argument("--sim-only",
-                    action="store_true",
-                    help="Will only simulate, not perform other steps.")
-parser.add_argument("--sim-render-only",
-                    action="store_true",
-                    help="Will only simulate and render, not perform other steps.")
-parser.add_argument("--render-only",
-                    action="store_true",
-                    help="Will only render, not perform other steps.")
-parser.add_argument("--compress-only",
-                    action="store_true",
-                    help="Will only compress images, not perform other steps.")
-parser.add_argument("--compress-reconstruct-only",
-                    action="store_true",
-                    help="Will only compress images and reconstruct 3D model, not perform other steps.")
-parser.add_argument("--reconstruct-only",
-                    action="store_true",
-                    help="Will only reconstruct 3D, not perform other steps.")
-parser.add_argument("--profile",
-                    action="store_true",
-                    help="Use cProfiler and write results to log.")
 
-def read_input(args):
+def read_input():
     """Read input, either from CLI or from input file"""
+    args = read_input_cli()
 
-    if args.cli:
-        settings = read_input_cli()
-    else:
-        settings = read_input_file(args)
+    inputfile = _parse_input_filepath(args.i)
+
+    settings = read_input_file(inputfile)
+
+    if "flags" in settings:
+        settings = _parse_input_flags(settings)
+
+    settings = parse_input(settings)
+
+    if args.o is not None:
+        res_dir = Path(args.o).resolve()
+        res_dir = utils.check_dir(res_dir)
+
+        settings["res_dir"] = res_dir
+
+    if args.n is not None:
+        settings["name"] = args.n
 
     return settings
 
 
 def read_input_cli():
     """
-    Reads input interactively from CLI.
+    Reads input from CLI.
+
+    Represents top level input of a definition.json file
     """
-    raise NotImplementedError()
+    cli_parser = argparse.ArgumentParser(description=__file__.__doc__)
+    cli_parser.add_argument("-i",
+                            action="store",
+                            default=None,
+                            type=str,
+                            help="Path to 'definition.json' file")
+    cli_parser.add_argument("-o",
+                            action="store",
+                            default=None,
+                            type=str,
+                            help="Path to results directory")
+    cli_parser.add_argument("-n",
+                            action="store",
+                            default=None,
+                            type=str,
+                            help="Name of simulation scenario")
+
+    cli_args = cli_parser.parse_args()
+
+    return cli_args
 
 
-def read_input_file(args):
+def read_input_file(filename):
     """
     Reads input from a given file.
 
     :type filename: String
     :param filename: Filename of a mission definition file.
     """
-    if args.i is None:
-        root_dir = Path(__file__).resolve().parent.parent
-        def_file = root_dir / "data" / "input" / "definition.json"
-    else:
-        def_file = Path(args.i).resolve()
+    with open(str(filename), "r") as def_file:
+        settings = json.load(def_file)
 
-        if not def_file.exists():
-            root_dir = Path(__file__).resolve().parent.parent
-            def_file = root_dir / "data" / "input" / args.i.name
+    return settings
 
-    with open(str(def_file), "r") as cfg_file:
-        settings = json.load(cfg_file)
 
-    return parse_settings_file(args, settings)
-
-def parse_settings_file(args, settings):
+def parse_input(settings):
     """
     Parses settings from input file or CLI into correct data formats
 
@@ -145,16 +114,73 @@ def parse_settings_file(args, settings):
 
     if "compression" not in settings:
         logger.debug("No compression settings provided!")
-    
+
     if "reconstruction" not in settings:
         logger.debug("No reconstruction settings provided!")
 
-    _parse_paths(settings)
-
-    if "flags" in settings:
-        parser.parse_args(args=settings["flags"], namespace=args)
+    settings = _parse_paths(settings)
+    settings = _parse_flags(settings)
 
     return settings
+
+
+def _parse_input_flags(settings):
+    """
+    Parse flags field in input
+    """
+    parser = argparse.ArgumentParser(description=__file__.__doc__)
+    parser.add_argument("-v",
+                        action="store_false",
+                        help="Verbose output, displays log also on STDOUT")
+    parser.add_argument("--no-sim",
+                        action="store_const",
+                        const=False,
+                        default=True,
+                        dest="with_sim",
+                        help="If set, sispo will not simulate the scenario")
+    parser.add_argument("--no-render",
+                        action="store_const",
+                        const=False,
+                        default=True,
+                        dest="with_render",
+                        help="If set, sispo will not render the scenario")
+    parser.add_argument("--no-compression",
+                        action="store_const",
+                        const=False,
+                        default=True,
+                        dest="with_compression",
+                        help="If set, images will not be compressed after rendering")
+    parser.add_argument("--no-reconstruction",
+                        action="store_const",
+                        const=False,
+                        default=True,
+                        dest="with_reconstruction",
+                        help="If set, no 3D model will be reconstructed")
+    parser.add_argument("--sim-only",
+                        action="store_true",
+                        help="Will only simulate, not perform other steps.")
+    parser.add_argument("--sim-render-only",
+                        action="store_true",
+                        help="Will only simulate and render, not perform other steps.")
+    parser.add_argument("--render-only",
+                        action="store_true",
+                        help="Will only render, not perform other steps.")
+    parser.add_argument("--compress-only",
+                        action="store_true",
+                        help="Will only compress images, not perform other steps.")
+    parser.add_argument("--compress-reconstruct-only",
+                        action="store_true",
+                        help="Will only compress images and reconstruct 3D model, not perform other steps.")
+    parser.add_argument("--reconstruct-only",
+                        action="store_true",
+                        help="Will only reconstruct 3D, not perform other steps.")
+    parser.add_argument("--profile",
+                        action="store_true",
+                        help="Use cProfiler and write results to log.")
+    settings["flags"] = parser.parse_args(args=settings["flags"])
+
+    return settings
+
 
 def _parse_paths(settings):
     """
@@ -183,6 +209,7 @@ def _parse_paths(settings):
 
     return settings
 
+
 def _parse_flags(settings):
     """
     Recursively parses all settings containing with_ prefix to a bool.
@@ -194,25 +221,38 @@ def _parse_flags(settings):
         if "with" in key:
             settings[key] = bool(settings[key])
         elif isinstance(settings[key], dict):
-            settings[key] = _parse_flags[settings[key]]
+            settings[key] = _parse_flags(settings[key])
 
     return settings
+
+
+def _parse_input_filepath(filepath):
+    if filepath is None:
+        root_dir = Path(__file__).resolve().parent.parent
+        filename = root_dir / "data" / "input" / "definition.json"
+    else:
+        filename = Path(filepath).resolve()
+
+        if not filename.exists():
+            root_dir = Path(__file__).resolve().parent.parent
+            filename = root_dir / "data" / "input" / filepath.name
+
+    return filename
+
 
 def main():
     """
     Main function to run when executing file
     """
-    args = parser.parse_args()
+    settings = read_input()
 
-    if args.v:
+    if settings["flags"].v:
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setLevel(logging.DEBUG)
         stream_handler.setFormatter(logger_formatter)
         logger.addHandler(stream_handler)
-    
-    settings = read_input(args)
 
-    if args.profile:
+    if settings["flags"].profile:
         pr = cProfile.Profile()
 
     now = datetime.now().strftime("%Y-%m-%dT%H%M%S%z")
@@ -225,7 +265,8 @@ def main():
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(logger_formatter)
     logger.addHandler(file_handler)
-    logger.debug("\n\n#################### NEW SISPO LOG ####################\n")
+    logger.debug(
+        "\n\n#################### NEW SISPO LOG ####################\n")
 
     logger.debug("Settings:")
     logger.debug(f"{settings}")
@@ -233,20 +274,20 @@ def main():
     comp_settings = settings["compression"]
     recon_settings = settings["reconstruction"]
 
-    if args.profile:
+    if settings["flags"].profile:
         logger.debug("Start Profiling")
         pr.enable()
 
     t_start = time.time()
-    
-    if args.sim_only:
+
+    if settings["flags"].sim_only:
         logger.debug("Only simulating, no other step")
         env = Environment(**sim_settings, ext_logger=logger)
         env.simulate()
         logger.debug("Finished simulating")
         return
-    
-    if args.sim_render_only:
+
+    if settings["flags"].sim_render_only:
         logger.debug("Only simulating and rendering, no other step")
         env = Environment(**sim_settings, ext_logger=logger)
         env.simulate()
@@ -254,7 +295,7 @@ def main():
         logger.debug("Finished simulating and rendering")
         return
 
-    if args.render_only:
+    if settings["flags"].render_only:
         raise NotImplementedError()
         logger.debug("Only rendering, no other step")
         env = Environment(**sim_settings, ext_logger=logger)
@@ -262,14 +303,14 @@ def main():
         logger.debug("Finished rendering")
         return
 
-    if args.compress_only:
+    if settings["flags"].compress_only:
         logger.debug("Only compressing, no other step")
         comp = Compressor(**comp_settings, ext_logger=logger)
         comp.comp_decomp_series()
         logger.debug("Finished compressing")
         return
 
-    if args.compress_reconstruct_only:
+    if settings["flags"].compress_reconstruct_only:
         logger.debug("Only compressing and reconstructing, no other step.")
         logger.debug("Start compressing")
         comp = Compressor(**comp_settings, ext_logger=logger)
@@ -281,38 +322,38 @@ def main():
         logger.debug("Finished reconstructing")
         return
 
-    if args.reconstruct_only:
+    if settings["flags"].reconstruct_only:
         logger.debug("Only reconstructing, no other step")
         recon = Reconstructor(**recon_settings, ext_logger=logger)
         recon.reconstruct()
         logger.debug("Finished reconstructing")
         return
-    
+
     logger.debug("Run full pipeline")
 
-    if args.with_sim or args.with_render:
+    if settings["flags"].with_sim or args.with_render:
         logger.debug("With either simulation or rendering")
         env = Environment(**sim_settings, ext_logger=logger)
-    
-        if args.with_sim:
+
+        if settings["flags"].with_sim:
             env.simulate()
-        
-        if args.with_render:
+
+        if settings["flags"].with_render:
             env.render()
 
-    if args.with_compression:
+    if settings["flags"].with_compression:
         logger.debug("With compression")
         comp = Compressor(**comp_settings, ext_logger=logger)
         comp.comp_decomp_series()
 
-    if args.with_reconstruction:
+    if settings["flags"].with_reconstruction:
         logger.debug("With reconstruction")
         recon = Reconstructor(**recon_settings, ext_logger=logger)
         recon.reconstruct()
 
     t_end = time.time()
-    
-    if args.profile:
+
+    if settings["flags"].profile:
         pr.disable()
         logger.debug("Stop Profile")
 
@@ -320,17 +361,19 @@ def main():
         sortby = pstats.SortKey.CUMULATIVE
         ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
         ps.print_stats()
-    
+
         logger.debug("\n##################### Pstats #####################\n")
         logger.debug("\n" + s.getvalue() + "\n")
         logger.debug("\n##################################################\n")
-    
+
     logger.debug(f"Total time: {t_end - t_start} s")
     logger.debug("Finished sispo main")
+
 
 def run():
     """Alias for :py:func:`main` ."""
     main()
+
 
 def change_arg(arg):
     """Change an argument in the argparser namespace."""
