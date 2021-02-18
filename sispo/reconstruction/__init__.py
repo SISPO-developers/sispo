dfdbf29ce678199f9ff875e1a1a3e7414f267333
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2021-02-18 21:59:17 +0100

    Fix init files, now structure like numpy

--- sispo/reconstruction/__init__.py
+++ sispo/reconstruction/__init__.py
@@ -1,2 +1,4 @@
 # To get sub-modules
+from .openmvg import *
+from .openmvs import *
 from .reconstruction import *
--- sispo/sim/__init__.py
+++ sispo/sim/__init__.py
@@ -1,2 +1,7 @@
 # To get sub-modules
+from .compositor import *
+from .render import *
+from .sc import *
 from .sim import *
+from .sssb import *
+from .starcat import *
