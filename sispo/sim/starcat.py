4ce4b7a5c7de25bb8a52e492e3425defe188b3ba
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2021-02-19 23:19:09 +0100

    Adaptive both starcat versions, default vizier

--- sispo/sim/starcat.py
+++ sispo/sim/starcat.py
@@ -21,16 +21,13 @@ class StarCatalog:
 
     def __init__(self, res_dir, ext_logger, starcat_dir=None):
         """."""
-
-        self.catalog = Vizier(catalog="UCAC4", row_limit=1000000)
-
         self.logger = ext_logger
 
-        self.root_dir = Path(__file__).parent.parent.parent
-
         if starcat_dir is None:
-            self.starcat_dir = self.root_dir / "data" / "UCAC4"
+            self.catalog = Vizier(catalog="UCAC4", row_limit=1000000)
         else:
+            self.root_dir = Path(__file__).parent.parent.parent
+
             starcat_dir = Path(starcat_dir)
 
             try:
@@ -39,40 +36,43 @@ class StarCatalog:
                 raise StarCatalogError(e)
 
             if not starcat_dir.is_dir():
-                starcat_dir = self.models_dir / starcat_dir.name
-                starcat_dir = starcat_dir.resolve()
-
-            if not starcat_dir.is_dir():
                 raise StarCatalogError("Given star cat dir does not exist.")
-        self.starcat_dir = starcat_dir
+            self.starcat_dir = starcat_dir
 
-        self.res_dir = res_dir
+            self.res_dir = res_dir
 
-        exe_dir = self.root_dir / "software" / "star_cats"
+            exe_dir = self.root_dir / "software" / "star_cats"
 
-        if (exe_dir / "u4test").is_file() or (exe_dir / "u4test.exe").is_file():
-            self.exe = exe_dir / "u4test"
-        elif ((exe_dir / "star_cats" / "u4test").is_file() or
-                (exe_dir / "star_cats" / "u4test.exe").is_file()):
-            self.exe = exe_dir / "star_cats" / "u4test"
-        elif ((exe_dir / "build_star_cats" / "u4test").is_file() or
-                (exe_dir / "build_star_cats" / "u4test.exe").is_file()):
-            self.exe = exe_dir / "build_star_cats" / "u4test"
-        else:
-            raise StarCatalogError("UCAC4 interface could not be found.")
+            if (exe_dir / "u4test").is_file() or (exe_dir / "u4test.exe").is_file():
+                self.exe = exe_dir / "u4test"
+            elif ((exe_dir / "star_cats" / "u4test").is_file() or
+                    (exe_dir / "star_cats" / "u4test.exe").is_file()):
+                self.exe = exe_dir / "star_cats" / "u4test"
+            elif ((exe_dir / "build_star_cats" / "u4test").is_file() or
+                    (exe_dir / "build_star_cats" / "u4test.exe").is_file()):
+                self.exe = exe_dir / "build_star_cats" / "u4test"
+            else:
+                raise StarCatalogError("UCAC4 interface could not be found.")
 
-        # Don't display the Windows GPF dialog if the invoked program dies.
-        # See comp.os.ms-windows.programmer.win32
-        # How to suppress crash notification dialog?, Jan 14,2004 -
-        # Raymond Chen"s response [1]
-        if sys.platform.startswith("win"):
-            self.logger.debug("Windows system, surrpressing GPF dialog.")
-            import ctypes
+            # Don't display the Windows GPF dialog if the invoked program dies.
+            # See comp.os.ms-windows.programmer.win32
+            # How to suppress crash notification dialog?, Jan 14,2004 -
+            # Raymond Chen"s response [1]
+            if sys.platform.startswith("win"):
+                self.logger.debug("Windows system, surrpressing GPF dialog.")
+                import ctypes
 
-            SEM_NOGPFAULTERRORBOX = 0x0002  # From MSDN
-            ctypes.windll.kernel32.SetErrorMode(SEM_NOGPFAULTERRORBOX)
-
+                SEM_NOGPFAULTERRORBOX = 0x0002  # From MSDN
+                ctypes.windll.kernel32.SetErrorMode(SEM_NOGPFAULTERRORBOX)
+        
     def get_stardata(self, ra, dec, width, height, filename="ucac4.txt"):
+        """."""
+        if hasattr(self, "catalog"):
+            return self.get_stardata_vizier(ra, dec, width, height)
+        else:
+            return self.get_stardata_ucac4(ra, dec, width, height, filename)
+
+    def get_stardata_ucac4(self, ra, dec, width, height, filename="ucac4.txt"):
         """Retrieve star data from given field of view using UCAC4 catalog."""
         res_file = self.res_dir / filename
         res_file = res_file.with_suffix(".txt")
@@ -119,13 +119,19 @@ class StarCatalog:
         """
 
         crds = coord.SkyCoord(
-            ra=ra, dec=dec, unit=(u.deg, u.deg), frame='icrs')
+            ra=ra, dec=dec, unit=(u.deg, u.deg))
         result = self.catalog.query_region(
             crds, width=width*u.deg, height=height*u.deg)[0]
 
         star_data = zip(result['RAJ2000'], result['DEJ2000'], result['f.mag'])
+        star_data = [(ra, de, mag) for ra, de, mag in star_data]
 
-        return [(ra, de, mag) for ra, de, mag in star_data]
+        star_res = []
+        for star in star_data:
+            if star[0] >= ra - width/2 and star[0] <= ra + width/2 and star[1] >= dec - height/2 and star[1] <= dec + height/2:
+                star_res.append(star)
+
+        return star_res
 
 
 # class StarCache:
